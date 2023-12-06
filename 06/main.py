cf3428c44db3c8a3513dee05c715cdd6715f3058
gnusij <gnusij@duck.com>
2023-12-06 12:47:47 -0500

    day06

--- 06/main.py
+++ 06/main.py
@@ -1,4 +1,3 @@
-from math import prod
-T,D = map(lambda x:[*map(int,x.split(":")[1].split())], open(0).readlines())
-f = lambda T,D:sum([1 if t * (T-t) > D else 0 for t in range(T+1)])
-print(prod([f(T[i],D[i]) for i in range(len(T))]), f(*map(lambda x:int(''.join(map(str,x))), [T,D])))
+d=[*map(lambda x:x.split(':')[1].split(),open(0).readlines())]
+f=lambda T,D:str(sum([1 for t in range(int(T)+1) if t*(int(T)-t)>int(D)]))
+print(eval('*'.join([f(*i) for i in zip(*d)])),f(*map(lambda x:int(''.join(x)),d)))
