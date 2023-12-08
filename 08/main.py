ec4aa0d5a889d2c7380c430f896a567e31e25492
gnusij <gnusij@duck.com>
2023-12-08 02:20:50 -0500

    day08

--- 08/main.py
+++ 08/main.py
@@ -1,11 +1,7 @@
-from math import lcm
-d=open(0).read().splitlines()
-I,G=[0 if c=='L' else 1 for c in d[0]],{k:n[1:-1].split(', ') for k,n in [l.split(' = ') for l in d[2:]]}
-def f(p,c):
-    i=1 
-    while i:
-        p=G[p][I[(i-1)%len(I)]]
-        if c(p): 
-            return i
-        i+=1
-print(f('AAA',lambda x:x=='ZZZ'),lcm(*[f(p,lambda x:x[2]=='Z') for p in [p for p in G if p[2]=='A']]))
+from math import*
+I,_,*G=open(0).read().splitlines()
+I,G=[0 if c=='L' else 1 for c in I],{k:n[1:-1].split(', ') for k,n in [l.split(' = ') for l in G]}
+def f(p,c,i=1):
+    while~-c(p:=G[p][I[(i-1)%len(I)]]):i+=1
+    return i
+print(f('AAA',lambda x:x=='ZZZ'),lcm(*[f(p,lambda x:x[2]=='Z') for p in G if p[2]=='A']))
