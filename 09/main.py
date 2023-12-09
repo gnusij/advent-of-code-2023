ed3886de5869fc5f940d1b3d375be2aaceed6328
gnusij <gnusij@duck.com>
2023-12-09 01:27:52 -0500

    day09

--- 09/main.py
+++ 09/main.py
@@ -1,10 +1,10 @@
-d=[[*map(int, l.split())] for l in open(0).read().splitlines()]
+d=[[*map(int, l.split())] for l in open(0).read().split('\n')]
 s,S=0,0
 for n in d:
     Q=[n]
     while any(n:=[n[i]-n[i-1] for i in range(1,len(n))]):
         Q+=[n]
-    s+=sum([n[-1] for n in Q])
+    s+=sum([q[-1] for q in Q])
     t=0
     for i in range(len(Q))[::-1]:
         t=Q[i][0]-t
