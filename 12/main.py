78e53b1773fe0cb1700a1c61e8c2c152c0571deb
gnusij <gnusij@duck.com>
2023-12-14 01:48:47 -0500

    day12 p2 try

--- 12/main.py
+++ 12/main.py
@@ -1,6 +1,6 @@
 import itertools
 
-d = open(0).read().splitlines()
+d = open("example.txt").read().splitlines()
 
 from math import comb
 
@@ -47,17 +47,106 @@ for line in d:
 
 print(s)
 
-# for line in d:
-#     record = line.split()[0]
-#     record = '.'.join([t for t in record.split('.') if t])
-#     nums = [*map(int, line.split()[1].split(','))]
-#     record = '?'.join(record for _ in range(5))
-#     n = []
-#     for i in range(5):
-#         n.extend(nums)
-#     nums = n  
-#     #n = find(record, nums)
-#     print(record, nums)
-#     #s += n
+for line in d:
+    record = line.split()[0]
+    nums = [*map(int, line.split()[1].split(','))]
+    record = '?'.join(record for _ in range(5))
+    record = '.'.join([t for t in record.split('.') if t])
+    n = []
+    for i in range(5):
+        n.extend(nums)
+    nums = n  
+    #n = find(record, nums)
+    #print(record, nums)
+    #s += n
 
 
+
+def study(record, num):
+
+    # find all 
+    pass
+
+
+def drop(record, num):
+    # first-last starts with #
+    txt = [t for t in record] 
+
+    if txt[0]==".": txt.pop(0)
+    if txt[-1]==".": txt.pop(-1)
+
+    if txt[0]=="#" and "." not in txt[0:num[0]]:
+        txt = txt[num[0]+1:]
+        num.pop(0)
+
+    elif txt[-1]=="#" and "." not in txt[len(txt)-num[-1]:len(txt)]:
+        txt = txt[:len(txt)-num[-1]-1]
+        num.pop(-1)
+
+    # first-last starts with ? but . in the way
+    elif txt[0]=='?' and '.' in txt[0:num[0]]:
+        txt = txt[''.join(txt).find('.')+1:]
+
+    elif txt[-1]=='?' and '.' in txt[len(txt)-num[-1]:len(txt)]:
+        txt = txt[:''.join(txt).rfind('.')-2]
+
+    # first letter ?, next #, and num[0] = 1
+    elif txt[0]=='?' and txt[1]=='#' and num[0]==1:
+        txt = txt[3:]
+        num = num[1:] 
+    
+    # only 1 possibility based on length 
+    if len(txt) == sum(num)+len(num)-1:
+        txt = []
+        num = []
+
+    # only 1 possibility based on num: e.g. ???. [1, 1, ...] 
+    if "#." not in txt and len(txt) == sum(num)+1:
+        txt = txt[sum(num)+1:]
+        num = [] 
+
+    # there is .#####.  
+    tmp = []
+    for t in ''.join(txt).split('.'):
+        if t == "#"*len(t) and len(t) in num:
+            num.remove(len(t))
+        else:
+            tmp.append(t)
+    txt = [t for t in '.'.join(tmp)]
+
+
+    if txt == []:
+        return txt, num
+
+    if txt[0]=="#" or txt[-1]=="#":
+        txt, num = drop(txt, num)   
+
+    print(txt, num)
+    return txt, num
+
+
+def firstlast(line):
+    record = line.split()[0]
+    record = '?'.join(record for _ in range(5))
+    record = '.'.join([t for t in record.split('.') if t])
+    num = [*map(int, line.split()[1].split(','))]
+    n = []
+    for i in range(5):
+        n.extend(num)
+    num = n  
+
+    #record = line.split()[0]
+    #num = [*map(int, line.split()[1].split(','))]
+    c = 1
+
+    print(record, num)
+    txt, num = drop(record, num)
+    print(txt, num)
+        
+    n = find(''.join(txt), num)
+    print(n)
+        
+    if array_equal([len(x) for x in ''.join([t for t in txt if t!='?']).split('.') if x], num):
+        return c
+    
+print(firstlast('????.######..#####. 1,6,5'))
