60140944221bc5b0b2c636185490ad952f95f5a6
gnusij <gnusij@duck.com>
2023-12-14 01:41:36 -0500

    day14

--- 14/main.py
+++ 14/main.py
@@ -1,88 +1,40 @@
 d = [[t for t in l] for l in open(0).read().split()]
 
-def t(d):
+def T(d):
     return [[d[i][j] for i in range(len(d))] for j in range(len(d[0]))]
 
-def N(d):
+def M(d,t=0,m=1):
     changed=0
-    for i in range(1,len(d))[::-1]:
+    if t:d = T(d)                   # W E
+    if m!=1: R=range(1,len(d))[::-1] # N W
+    else: R=range(len(d)-1)          # S E
+    for i in R:
         for j in range(len(d[0])):
-            if d[i][j] == "O" and d[i-1][j] not in "#O":
-                d[i-1][j] = "O"
+            if d[i][j] == "O" and d[i+m][j] not in "#O":
+                d[i+m][j] = "O"
                 d[i][j] = "."
                 changed=1
+    if t:d = T(d)
     if changed:
-        return N(d)
+        return M(d,t,m)
     else:
         return d
-
-def W(d):
-    d = t(d)
-    changed=0
-    for i in range(1,len(d))[::-1]:
-        for j in range(len(d[0])):
-            if d[i][j] == "O" and d[i-1][j] not in "#O":
-                d[i-1][j] = "O"
-                d[i][j] = "."
-                changed=1
-    if changed:
-        return W(t(d))
-    else:
-        return t(d)
-
-def S(d):
-    changed=0
-    for i in range(len(d)-1):
-        for j in range(len(d[0])):
-            if d[i][j] == "O" and d[i+1][j] not in "#O":
-                d[i+1][j] = "O"
-                d[i][j] = "."
-                changed=1
-    if changed:
-        return S(d)
-    else:
-        return d
-
-def E(d):
-    d = t(d)
-    changed=0
-    for i in range(len(d)-1):
-        for j in range(len(d[0])):
-            if d[i][j] == "O" and d[i+1][j] not in "#O":
-                d[i+1][j] = "O"
-                d[i][j] = "."
-                changed=1
-    if changed:
-        return E(t(d))
-    else:
-        return t(d)
 
 def load(d):
-    load = 0
+    l = 0
     for i in range(len(d)):
         for j in range(len(d[0])):
             if d[::-1][i][j] == "O":
-                load += i+1 
-    return load
+                l += i+1 
+    return l
 
-print(load(N(d)))
+print(load(M(d,0,-1)))
 
-def cycle(d):
-    d = N(d)
-    d = W(d)
-    d = S(d)
-    d = E(d)
-    return d
-
-seen = [] 
-orig = ''.join([''.join(l) for l in d])
-seen.append(orig)
-
+seen = [''.join([''.join(l) for l in d])] 
 i = 1
 while True:
-    d = cycle(d)
+    d = M(M(M(M(d,0,-1),1,-1),0,1),1,1)
     txt = ''.join([''.join(l) for l in d])
-
     if txt in seen:
         start = seen.index(txt)
         repeat = i-seen.index(txt)
@@ -90,8 +42,5 @@ while True:
     else:
         seen.append(txt)
     i += 1
-
-for i in range((1000000000-start)%repeat):
-    d = cycle(d)
-
+d = [seen[start+(1000000000-start)%repeat][i:i+len(d[0])] for i in range(0, len(seen[0]), len(d[0]))]
 print(load(d))
